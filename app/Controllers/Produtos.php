5250f6cd392211b1c917bf25568783ea157efa17
Darlei Andre Lippi <darleilippi@uri.edu.br>
2021-06-30 14:29:22 -0300

    Adição dos controllers, models e view dos Produtos

---
+++ app/Controllers/Produtos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Produtos extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
---
+++ app/Models/ProdutoModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProdutoModel extends Model
+{
+    public function cadastraRegistro()
+    {
+        return 'Módulo responsável por cadastrar todos os produtos!';
+    }
+}
